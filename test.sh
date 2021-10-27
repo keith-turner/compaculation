502645ce1393d3b126d97d28f7d07110a9748066
Keith Turner <keith@deenlo.com>
2021-10-27 09:15:52 -0400

    Remove test script that is no longer relevant

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mvn clean compile
-java -cp target/classes compaculation.Main 3 NEW > results_3_NEW.txt
-java -cp target/classes compaculation.Main 3 OLD > results_3_OLD.txt
-java -cp target/classes compaculation.Main 2 NEW > results_2_NEW.txt
-java -cp target/classes compaculation.Main 2 OLD > results_2_OLD.txt
-java -cp target/classes compaculation.Main 1.5 NEW > results_1.5_NEW.txt
-java -cp target/classes compaculation.Main 1.5 OLD > results_1.5_OLD.txt
